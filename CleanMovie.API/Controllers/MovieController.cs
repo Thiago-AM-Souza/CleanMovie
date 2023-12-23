d5ca7504fd018988495e024a0245f9b5373a75e6
thiago-am-souza <thiagosouza96.ts@gmail.com>
2023-12-23 20:33:30 -0300

    Adicionar arquivos de projeto.

---
+++ CleanMovie.API/Controllers/MovieController.cs
@@ -0,0 +1,29 @@
+﻿using CleanMovie.Application;
+using CleanMovie.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CleanMovie.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private readonly IMovieService _movieService;
+
+        public MovieController(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Movie>> Get()
+        {
+            var moviesFromService = _movieService.GetAllMovies();
+
+
+            return Ok(moviesFromService);
+        }
+    }
+}
---
+++ CleanMovie.Infra/MovieRepository.cs
@@ -0,0 +1,24 @@
+﻿using CleanMovie.Application;
+using CleanMovie.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanMovie.Infra
+{
+    public class MovieRepository : IMovieRepository
+    {
+        public static List<Movie> Movies = new List<Movie>()
+        {
+            new Movie {Id = 1, Name="Avengers", Cost=19.9m},
+            new Movie {Id = 2, Name="Equalizer", Cost=29.9m},
+        };
+
+        public List<Movie> GetAllMovies()
+        {
+            return Movies;
+        }
+    }
+}
